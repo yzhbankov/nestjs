6a7220601c93b0104e7375b40d46800b7c3075f9
yzhbankov <yzhbankov@gmail.com>
2019-08-03 16:29:38 +0300

    add filter

---
+++ src/common/filter/http-exception.filter.ts
@@ -0,0 +1,21 @@
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException } from '@nestjs/common';
+import { HttpArgumentsHost } from '@nestjs/common/interfaces';
+import { Request, Response } from 'express';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+    public catch(exception: HttpException, host: ArgumentsHost): any {
+        const ctx: HttpArgumentsHost = host.switchToHttp();
+        const response: Response = ctx.getResponse<Response>();
+        const request: Request = ctx.getRequest<Request>();
+        const status: number = exception.getStatus();
+
+        response
+            .status(status)
+            .json({
+                statusCode: status,
+                timeStamp: new Date(),
+                path: request.url,
+            });
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 
 import { ApplicationModule } from './app.module';
 import { globalLoggerMiddleware } from './common/middleware/logger.middleware';
+import { HttpExceptionFilter } from './common/filter/http-exception.filter';
 
 async function bootstrap(): Promise<void> {
     const app: NestExpressApplication = await NestFactory.create<NestExpressApplication>(ApplicationModule);
     app.use(globalLoggerMiddleware);
+    app.useGlobalFilters(new HttpExceptionFilter());
     await app.listen(3000);
 }
 
